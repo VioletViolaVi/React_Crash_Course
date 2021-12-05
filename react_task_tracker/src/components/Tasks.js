2e7219c533327589538b20137466a6bf9854a371
vivian chinweze <vivian.v18723@gmail.com>
2021-12-05 16:42:17 +0000

    Add chores to task tracker.

--- react_task_tracker/src/components/Tasks.js
+++ react_task_tracker/src/components/Tasks.js
@@ -1,29 +1,44 @@
 // shortcut: rafce (delete [import React from 'react'] above)
-    const tasks = [
-        {
-            id: 1,
-            jobTask: "Clean laptop", 
-            deadline: "23rd Dec 2021",
-            reminder: true
-        },
-        {
-            id: 2,
-            jobTask: "Do laundry", 
-            deadline: "16th Dec 2021",
-            reminder: false
-        },
-        {
-            id: 3,
-            jobTask: "Throw away bin", 
-            deadline: "10th Dec 2021",
-            reminder: true
-        }
-    ]
+
+import { useState } from "react";
 
 const Tasks = () => {
+    const [chores, setChores] = useState(
+        [
+            {
+                id: 1,
+                jobChore: "Clean laptop", 
+                deadline: "23rd Dec 2021",
+                reminder: true
+            },
+            {
+                id: 2,
+                jobChore: "Do laundry", 
+                deadline: "16th Dec 2021",
+                reminder: false
+            },
+            {
+                id: 3,
+                jobChore: "Throw away bin", 
+                deadline: "10th Dec 2021",
+                reminder: true
+            }, 
+            {
+                id: 4,
+                jobChore: "Eat fruit",
+                deadline: "5th Dec 2021",
+                reminder: true
+            }
+        ]
+    )
     return (
         <div>
-            
+            {
+                chores.map(chore => 
+                <h3 key={chore.id}>
+                    {chore.jobChore}
+                </h3>)
+            }
         </div>
     )
 }
