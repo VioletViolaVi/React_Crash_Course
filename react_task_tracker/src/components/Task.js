95d95c8b723586b7ba5fc6b2acc38377b8cbb6d0
vivian chinweze <vivian.v18723@gmail.com>
2021-12-05 20:24:39 +0000

    Style 'x' icons.

--- react_task_tracker/src/components/Task.js
+++ react_task_tracker/src/components/Task.js
@@ -1,7 +1,11 @@
+import {FaTimes} from "react-icons/fa";
+
 const Task = ({singleChore}) => {
     return (
         <div className="task">
-            <h3>{singleChore.jobChore}</h3>
+            <h3>
+                {singleChore.jobChore} <FaTimes style={{color: "red", cursor:"pointer"}} />
+            </h3>
             <p>{singleChore.deadline}</p>
             <p>Completed: {singleChore.reminder ? "YES" : "NO"}</p>
         </div>
