b362355c299bc020833e156c3d0a0f4c7e08fde6
vivian chinweze <vivian.v18723@gmail.com>
2021-12-05 16:51:25 +0000

    Move useState to App.js

--- react_task_tracker/src/App.js
+++ react_task_tracker/src/App.js
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [chores] = useState(
+    [
+      {
+          id: 1,
+          jobChore: "Clean laptop", 
+          deadline: "23rd Dec 2021",
+          reminder: true
+      },
+      {
+          id: 2,
+          jobChore: "Do laundry", 
+          deadline: "16th Dec 2021",
+          reminder: false
+      },
+      {
+          id: 3,
+          jobChore: "Throw away bin", 
+          deadline: "10th Dec 2021",
+          reminder: true
+      }, 
+      {
+          id: 4,
+          jobChore: "Eat fruit",
+          deadline: "5th Dec 2021",
+          reminder: true
+      }
+    ])
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks chores={chores}/>
     </div>
   );
 }
--- react_task_tracker/src/components/Tasks.js
+++ react_task_tracker/src/components/Tasks.js
@@ -1,40 +1,9 @@
 // shortcut: rafce (delete [import React from 'react'] above)
-
-import { useState } from "react";
-
-const Tasks = () => {
-    const [chores, setChores] = useState(
-        [
-            {
-                id: 1,
-                jobChore: "Clean laptop", 
-                deadline: "23rd Dec 2021",
-                reminder: true
-            },
-            {
-                id: 2,
-                jobChore: "Do laundry", 
-                deadline: "16th Dec 2021",
-                reminder: false
-            },
-            {
-                id: 3,
-                jobChore: "Throw away bin", 
-                deadline: "10th Dec 2021",
-                reminder: true
-            }, 
-            {
-                id: 4,
-                jobChore: "Eat fruit",
-                deadline: "5th Dec 2021",
-                reminder: true
-            }
-        ]
-    )
+const Tasks = ({chores}) => {
     return (
         <div>
             {
-                chores.map(chore => 
+                chores.map(chore =>
                 <h3 key={chore.id}>
                     {chore.jobChore}
                 </h3>)
