97d023de29b2ec6c6e7795e04530265ad2d5bdf2
vivian chinweze <vivian.v18723@gmail.com>
2021-12-05 16:09:58 +0000

    Create Tasks component and add task object array.

--- react_task_tracker/src/components/Header.js
+++ react_task_tracker/src/components/Header.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types' // 'impt' shortcut
+import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ myTitle }) => {
---
+++ react_task_tracker/src/components/Tasks.js
@@ -0,0 +1,31 @@
+// shortcut: rafce (delete [import React from 'react'] above)
+    const tasks = [
+        {
+            id: 1,
+            jobTask: "Clean laptop", 
+            deadline: "23rd Dec 2021",
+            reminder: true
+        },
+        {
+            id: 2,
+            jobTask: "Do laundry", 
+            deadline: "16th Dec 2021",
+            reminder: false
+        },
+        {
+            id: 3,
+            jobTask: "Throw away bin", 
+            deadline: "10th Dec 2021",
+            reminder: true
+        }
+    ]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
