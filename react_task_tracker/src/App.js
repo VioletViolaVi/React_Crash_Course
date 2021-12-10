b0c6967fc49c33034e3e7e9dbef355df8abf3d4f
vivian <vivian.v18723@gmail.com>
2021-12-10 19:42:40 +0000

    Transfer chores object to db.json file.

--- react_task_tracker/src/App.js
+++ react_task_tracker/src/App.js
@@ -5,33 +5,7 @@ import Tasks from "./components/Tasks";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [chores, setChores] = useState(
-    [
-      {
-          id: 1,
-          jobChore: "Clean laptop", 
-          deadline: "23rd Dec 2021",
-          reminder: true
-      },
-      {
-          id: 2,
-          jobChore: "Do laundry", 
-          deadline: "16th Dec 2021",
-          reminder: false
-      },
-      {
-          id: 3,
-          jobChore: "Throw away bin", 
-          deadline: "10th Dec 2021",
-          reminder: true
-      }, 
-      {
-          id: 4,
-          jobChore: "Eat fruit",
-          deadline: "5th Dec 2021",
-          reminder: true
-      }
-    ]);
+  const [chores, setChores] = useState([]);
 
     // ADDS TASK
     const addTask = (singleChore) => {
